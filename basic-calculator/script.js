9cb69579886eb87d4467fcb812f13fd06cb5accd
aswanth <aswanthshajicv@gmail.com>
2026-03-25 22:14:24 +0530

    basic calculator

---
+++ basic-calculator/script.js
@@ -0,0 +1,21 @@
+function btnClick(value){
+  let display  = document.getElementById("display");
+  let current = display.value;
+  let lastChar = current[current.length -1];
+
+  if((value === '/' || value === '%' || value === '*' || value === '-' || value === '+') && (lastChar === '/' || lastChar === '%' || lastChar === '*' || lastChar === '-' || lastChar === '+')){
+    return;
+}
+else{
+    document.getElementById("display").value += value;
+}  
+}
+
+function calculate(){
+  let expression = document.getElementById("display");
+  expression.value = eval(expression.value);
+}
+
+function clearDisplay() {
+  document.getElementById("display").value = "";
+}
